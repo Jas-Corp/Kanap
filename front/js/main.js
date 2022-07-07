ebe40176b58acebf4c54173704dba7c226b0e6f3
Jas <ponsjason125@gmail.com>
2022-07-07 21:49:07 +0200

    init

---
+++ front/js/main.js
@@ -0,0 +1,35 @@
+const products = document.getElementById("items");
+
+async function getProducts(url) {
+  const response = await fetch(url);
+  var data = await response.json();
+  return data;
+}
+
+function buildProductList(canapes) {
+  var productlist = "";
+
+  canapes.forEach(function (canape) {
+    productlist +=
+      '<a href="./product.html?id=' +
+      canape._id +
+      '"> <article> <img src="' +
+      canape.imageUrl +
+      '" alt="' +
+      canape.altTxt +
+      '> <h3 class="productName">' +
+      canape.name +
+      '</h3> <p class="productDescription">' +
+      canape.description +
+      "</p> </article> </a>";
+  });
+  
+  return productlist;
+}
+
+async function loadProducts() {
+  const canapes = await getProducts("http://localhost:3000/api/products");
+  products.innerHTML = buildProductList(canapes);
+}
+
+loadProducts();
